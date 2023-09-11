a837ec968feb6654a6362f2dafbfc97227e659f6
Gabriel Torres Martín <72462461+gabritorres@users.noreply.github.com>
2023-09-11 21:27:30 +0200

    Add files via upload

--- starter.py
+++ starter.py
@@ -1,4 +1,5 @@
 import pyodbc
+import time
 
 # Establecer conexión al DSN
 conn_str = 'DSN=MySQL_TFG'
@@ -16,12 +17,18 @@ while True:
         break
 
     try:
+        start_time = time.time()
         cursor.execute(query)
         try:
             for row in cursor:
-                print(row)
+                print(' '.join(map(str, row)))
+
         except pyodbc.ProgrammingError:
             print("La consulta se ejecutó con éxito pero no devolvió resultados.")
+
+        end_time = time.time()  # Registrar el tiempo final
+        elapsed_time = end_time - start_time  # Calcular el tiempo transcurrido
+        print(f"Tiempo transcurrido: {elapsed_time:.5f} segundos")  # Mostrar el tiempo transcurrido con 5 decimales
         
     except pyodbc.DatabaseError as e:
         print(f"Error en la base de datos: {e}")
